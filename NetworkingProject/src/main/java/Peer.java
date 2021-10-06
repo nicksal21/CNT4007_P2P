2753b34b5c3453160d6b12f9ababfe00610b5ad4
Alexandros Peltekis <44203626+AlexandrosPeltekis@users.noreply.github.com>
2021-10-06 19:04:03 -0400

    Peer object version 2

--- NetworkingProject/src/main/java/Peer.java
+++ NetworkingProject/src/main/java/Peer.java
@@ -7,16 +7,18 @@ public class Peer {
     int peerID;
     int listeningPort;
     boolean hasFile;
-    boolean isChoked;
+    int [][] filePieces;
 
     // This is the constructor of the class Employee
-    public Peer(int key, LinkedHashMap<Integer, String[]> peerInfo ) {
+    public Peer(int key, LinkedHashMap<Integer, String[]> peerInfo, LinkedHashMap<String, Integer> commonInfo ) {
         hostName = peerInfo.get(key)[1];
         listeningPort = Integer.parseInt(peerInfo.get(key)[2]);
 
-        hasFile = Integer.parseInt(peerInfo.get(key)[3]) == 1;
+        hasFile = Integer.parseInt(peerInfo.get(key)[3])== 1;
 
         isChoked = false;
+
+        setFilePieces(commonInfo.get("FilesSize"), commonInfo.get("PieceSize"));
     }
 
     // Method to choke or unchoke a peer
@@ -25,21 +27,24 @@ public class Peer {
         -Make this function apply to more than one peer
         use a list or map of some sort
      */
-    public void isChoked (boolean choked) {
+    public void setIsChoked (boolean choked) {
         isChoked = choked;
     }
 
-    /* Assign the designation to the variable designation.*/
+    private void setFilePieces (int fileSize, int pieceSize ) {
+        int numPieces = fileSize/pieceSize;
+        filePieces = new byte[numPieces][pieceSize];
+    }
+
     public int getListeningPort() {
         return listeningPort;
     }
 
-    /* Assign the salary to the variable	salary.*/
     public int getPeerID() {
         return peerID;
     }
 
-    /* Print the Employee details */
+    /* Print the Peer details */
     public void printPeerInfo() {
         System.out.println("The Hostname:"+ hostName );
         System.out.println("Peer ID:" + peerID );
