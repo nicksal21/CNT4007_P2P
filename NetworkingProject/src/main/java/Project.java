e61bd4d0e60bd9681b9003627b00faaf45bb10d0
Nicholas Salazar <nsalazar1@ufl.edu>
2021-10-21 18:34:33 -0400

    Tweaked Files

--- NetworkingProject/src/main/java/Project.java
+++ NetworkingProject/src/main/java/Project.java
@@ -1,21 +1,22 @@
 package main.java;
 // Bullock, Peltekis, & Salazar
+
 // Imports
-import java.io.DataInputStream;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.util.Arrays;
+import java.net.Socket;
+import java.util.*;
 import java.util.Scanner;
-import java.util.LinkedHashMap;
-import java.util.Set;
 
 class Project {
+
     /*
-    The readPeerInfo will take in the file path to the Peer information document.
-    This function will scan the file and place the information into a linked has map
-    which will be utilized to build our peer object.
+     * The readPeerInfo will take in the file path to the Peer information document.
+     * This function will scan the file and place the information into a linked hash map
+     * which will be utilized to build our peer object.
      */
     public static LinkedHashMap<Integer, String[]> readPeerInfo(String path) {
         LinkedHashMap<Integer, String[]> lhm = new LinkedHashMap<>();
@@ -25,22 +26,29 @@ class Project {
             File file = new File(path);
             Scanner input = new Scanner(file);
 
-            //Create the Map
-            while(input.hasNextLine()) {
-                int key;
+            /*
+             * PeerInfo.cfg Map Legend
+             * Key: Peer ID <int>
+             * Value: "Term" array that includes
+             *  [0] - Hostname
+             *  [1] - Port number
+             *  [2] - Peer has the file?
+             */
+            while (input.hasNextLine()) {
+                // Preprocessing
                 String[] term = new String[3];
                 String line = input.nextLine();
                 String[] lineArr = line.split(" ");
-                key = Integer.parseInt(lineArr[0]);
+                // Establish Map Entry
+                int key = Integer.parseInt(lineArr[0]);
                 term[0] = lineArr[1];
                 term[1] = lineArr[2];
                 term[2] = lineArr[3];
                 lhm.put(key, term);
             }
-        }
-        catch(Exception e) {
-            // Scream!
-            System.out.println("REEEEEEEE!!!  IT NO EXIST!!!  FEED ME PROPER PATH!!!");
+        } catch (Exception e) {
+            // Error Handling
+            System.out.println("Error!  Pathway does not exist!");
             e.getStackTrace();
         }
 
@@ -48,9 +56,9 @@ class Project {
     }
 
     /*
-    ReadCommon is identical to our readPeerInfo() function. This function will
-    scan the common.cfg file and save the information into a linked hash map
-    which will be utilized to configure our TCP network settings.
+     * ReadCommon is identical to our readPeerInfo() function. This function will
+     * scan the common.cfg file and save the information into a linked hash map
+     * which will be utilized to configure our TCP network settings.
      */
 
     public static LinkedHashMap<String, Integer> readCommon(String path) {
@@ -60,16 +68,27 @@ class Project {
             File file = new File(path);
             Scanner input = new Scanner(file);
 
-            //Create the Map
-            while(input.hasNextLine()) {
+            /*
+             * Common.cfg Map Legend
+             * Keys: <String>
+             *  - Number of Preferred Neighbors
+             *  - Unchoking Interval
+             *  - Optimistic Unchoking Interval
+             *  - File Name
+             *  - File Size
+             *  - Piece Size
+             * Value: [Respective <int> value]
+             */
+            while (input.hasNextLine()) {
+                // Preprocess
                 String key;
                 int term;
                 String line = input.nextLine();
                 String[] lineArr = line.split(" ");
 
+                // Establish Map Entry
                 key = lineArr[1];
                 term = Integer.parseInt(lineArr[1]);
-
                 lhm.put(key, term);
             }
         }
@@ -83,58 +102,97 @@ class Project {
     }
 
     /*
-    In order for the main to run properly User needs to enter the locations
-    of the PeerInfo.cfg and CommonInfo.cfg in that order. With this information
-    the main will use the information provided to run the read functions mentioned
-    previously. The Linked Hash Maps will then been utilized to buid the Peer Object
-    and TCP Network.
+     * In order for the main to run properly User needs to enter the locations
+     * of the PeerInfo.cfg and CommonInfo.cfg in that order. With this information
+     * the main will use the information provided to run the read functions mentioned
+     * previously. The Linked Hash Maps will then been utilized to buid the Peer Object
+     * and TCP Network.
      */
 
-    public static void main(String[]args){
+    public static void main(String[]args) throws IOException{
+        List<Peer> peersOnline = null;
         //Setup scanner for user input
         Scanner userInput = new Scanner(System.in);
 
-        //User inputs the path to the common.cfg
+        // User inputs the path to the PeerInfo.cfg
         String path = userInput.next();
 
-        //Read PeerInfo files
-        LinkedHashMap<Integer, String[]> PeerInfo  = readPeerInfo(path);
+        /*
+         * PeerInfo.cfg Map Legend
+         * Key - Peer ID <String>
+         * Value - "Term" array that includes
+         *  [0] - Hostname
+         *  [1] - Port number
+         *  [2] - Peer has the file?
+         */
+        LinkedHashMap<Integer, String[]> PeerInfo = readPeerInfo(path);
+
+        // User inputs the path to the Common.cfg
         path = userInput.next();
 
-        /*           CommonInfo Legend
-        Key = the peer ID
-        Map holds term array which is
-            0. computer's name
-            1. port number
-            2. If it has the complete file
+        /*
+         * Common.cfg Map Legend
+         * Keys: <String>
+         *  - Number of Preferred Neighbors
+         *  - Unchoking Interval
+         *  - Optimistic Unchoking Interval
+         *  - File Name
+         *  - File Size
+         *  - Piece Size
+         * Value: [Respective <int> value]
          */
         LinkedHashMap<String, Integer> CommonInfo = readCommon(path);
 
-        /*           PeerInfo Legend
-        Key = the peer ID
-        Map holds term array which is
-            0. computer's name
-            1. port number
-            2. If it has the complete file
-         */
         Set<Integer> keySet = PeerInfo.keySet();
         System.out.println(keySet);
+
         // Testing sample values
         System.out.println(Arrays.toString(PeerInfo.get(1001)));
         System.out.println(Arrays.toString(PeerInfo.get(1004)));
 
-
-        for(int i = 0; i <= 8; i++) {
+        // Test Print
+        for (int i = 0; i <= 8; i++) {
             int temp = 1001 + i;
             System.out.println(Arrays.toString(PeerInfo.get(temp)));
         }
 
         /*
-        TODO: Zeroth, FINISH CLIENT DESIGN
-        TODO: First, find a way to connect all peers to each other
-        TODO: Second, Make a holder for the peer object to hold connects / keep track of the connections
-        TODO: Third, Make an evaluator for information quality
-        TODO: Fourth, Test Cases.....
+         * This section here is responsible for creating both the server
+         * and client sockets. For now the client sockets will be left unconnected
+         * In the peer object we will create a private function that will connections
+         * to each of the hosts.
+         */
+
+        ServerSocket[] serverSockets = new ServerSocket[keySet.size()];
+        Socket[][] clientSockets = new Socket[keySet.size()][keySet.size()];
+
+        for(int i = 0; i < keySet.size(); i++) {
+            for (int j = 0; j < keySet.size() - 1; j++) {
+                Socket clientSocket = new Socket();
+                clientSockets[i][j] = clientSocket;
+            }
+            //ServerSocket serverSocket =
+            serverSockets[i] = new ServerSocket(Integer.parseInt(PeerInfo.get(1001+i)[1]));
+        }
+        /*
+         * This for loop is responsible for creating peer objects which
+         * will contain all the necessary information that our Peer-2-Peer
+         * network will be utilizing
+         */
+        for(int i = 0; i < keySet.size(); i++) {
+            peersOnline.add(new Peer(i, PeerInfo, CommonInfo));
+        }
+
+
+        /*
+         * To-do list
+         * TODO: Zeroth, FINISH CLIENT DESIGN
+         * TODO: First, find a way to connect all peers to each other
+         * TODO: Second, Make a holder for the peer object to hold connects / keep track of the connections
+         * TODO: Third, Make an evaluator for information quality
+         * TODO: Fourth, Test Cases.....
+         * TODO: Fifth,  Comment the SH*T out everything
+         * TODO: Sixth, StartRemotePeer???
          */
     }
 }
--- NetworkingProject/src/main/java/StartFileServer.java
+++ NetworkingProject/src/main/java/StartFileServer.java
@@ -1,4 +1,5 @@
 package main.java;// Bullock, Peltekis, & Salazar
+
 // Imports
 
 import java.io.*;
@@ -11,12 +12,10 @@ This class is has-a relationship with a peer ie a peer has severs.
 Peers act as both client as servers. Peer hosts will recieve information
 from each of the clients through the InputStream and write to the OutputFile.
  */
-
 public class StartFileServer { // https://www.baeldung.com/a-guide-to-java-sockets
-
     private List<ServerSocket> serverSockets;
     private ServerSocket current;
-    private LinkedHashMap<Integer, String[]>pInfo;
+    private LinkedHashMap<Integer, String[]> pInfo;
     private LinkedHashMap<String, Integer> cInfo;
 
     /*
@@ -27,14 +26,15 @@ public class StartFileServer { // https://www.baeldung.com/a-guide-to-java-socke
     public void start(LinkedHashMap<Integer, String[]> peerInfo, LinkedHashMap<String, Integer> commonInfo) {
         pInfo = peerInfo;
         cInfo = commonInfo;
-       //Keys will allow us to keep track of the size and location of the peers
+
+        // Keys will allow us to keep track of the size and location of the peers
         List<Integer> keys = new ArrayList<Integer>(peerInfo.keySet());
 
         // While loop will create each of the Server Sockets for the peers
         int i = keys.get(0);
         while (i < keys.get(keys.size() - 1)) {
-            try { // Try to create a server socket; if not, throw an IOException
-                // Create a
+            try {
+                // Create a server socket or throw an IOException
                 serverSockets.add(new ServerSocket(Integer.parseInt(peerInfo.get(i)[0]), Integer.parseInt(peerInfo.get(i)[1])));
                 current = serverSockets.get(i);
                 //EchoClientHander is a function below
@@ -61,12 +61,14 @@ public class StartFileServer { // https://www.baeldung.com/a-guide-to-java-socke
         private FileOutputStream out;
         private InputStream in;
 
-        public EchoClientHandler(Socket socket, String pname) throws IOException {
+        // Constructor
+        public EchoClientHandler(Socket socket, String pname) throws IOException{
             this.clientSocket = socket;
             out = new FileOutputStream("java/project_config_file_small/project_config_file_small/" + pname+ "/thefile");
             in = clientSocket.getInputStream();
         }
 
+        // Close all streams and sockets on peer exit
         public void exit(Socket clientSocket, InputStream in, FileOutputStream out) {
 
             try {
@@ -80,37 +82,37 @@ public class StartFileServer { // https://www.baeldung.com/a-guide-to-java-socke
 
         }
 
+        // Run peer
         public void run(Peer p) {
             try {
+                // Input and Output Streams
+                InputStream in = clientSocket.getInputStream();
+                FileOutputStream out = new FileOutputStream("java/project_config_file_small/project_config_file_small/" + pInfo.get(p.getPeerID())[0] + "/thefile");
 
                 // Number of bits based off the Common.cfg
                 byte[] b = new byte[cInfo.get("PieceSize")];
 
                 /*
-                Check if client peer is unchoked;
-                I.e., Client Peer is able to snd/rcv data
+                 * Check if client peer is unchoked;
+                 * I.e., Client Peer is able to transmit or receive data
                  */
-                while(!p.getChokedPeer()[p.getPeerID() - 1001]) {
-                    //Reads the input stream
+                while (!p.getChokedPeer()[p.getPeerID() - 1001]) {
+                    // Reads the input stream
                     in.read(b, 0, b.length);
-                    //Writes the File output stream
+                    // TODO: Use "b" so peer doesn't get stuck reading/writing from same location
+                    // Writes the File output stream
                     out.write(b, 0, b.length);
                 }
 
                 // Checks to see if the peer wants to close its connections
-                if(p.getWantToClose()) {
-                    exit(clientSocket,in, out);
+                if (p.getWantToClose()) {
+                    exit(clientSocket, in, out);
                 }
             }
-            /*
-            Need try catch to in order to handle the file stream
-             */
+            // Handle errors
             catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 }
-
-
-
